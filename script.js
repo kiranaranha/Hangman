a7385888e51b4ac1a80333fb5a76977af5971e6a
kiran aranha <kiranaranha@students.berkeley.net>
2017-11-28 11:39:49 -0800

    dknsdjjsdn

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 //global variables
 var word = '';
 var easyWords = ['cat', 'dog', 'mouse', 'horse', 'apple', 'banana', 'stop', 'lamp', 'tree', 'happy', 'life', 'dirt',
-    'computer'];
-var mediumWords = ['operating system', 'procrastination', 'lynx', 'solar system', 'watermelon', 'asparagus',
-    'sandwich'];
-var hardWords = ['endoplasmic reticulum', 'megalomaniac', 'xylophone', 'australopithecus', 'declaration',
-    'extraterrestrial', 'spaghetti', 'advantageous', 'iteration'];
+    'computer', 'butter', 'igloo', 'panda'];
+var mediumWords = ['system', 'procrastination', 'lynx', 'pineapple', 'watermelon', 'asparagus',
+    'sandwich', 'peanut', 'monkey', 'giraffe', 'forest'];
+var hardWords = ['chicken', 'megalomaniac', 'xylophone', 'australopithecus', 'declaration',
+    'extraterrestrial', 'spaghetti', 'advantageous', 'iteration', 'sausage', 'stupendous'];
 var guesses = 0;
 var wrongLetters = [];
 var letterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -47,17 +47,22 @@ function wordDisplay(){
 }
 
 function handleGuess(){
+    console.log(guesses);
     document.getElementById("message").innerHTML = '';
     if(wrongLetters.includes(document.getElementById("letterBox").value)){
         document.getElementById("message").innerHTML = 'You have already guessed this letter! Please choose another letter.';
     }else if(word.includes(document.getElementById("letterBox").value)){
         for(var i = 0; i < word.length; i++){
             if(word[i] == document.getElementById("letterBox").value){
-                dash.replaceAt(i, document.getElementById("letterBox").value);
+                dash = dash.substring(0,i) + document.getElementById("letterBox").value + dash.substring(i + 1, word.length);
             }
         }
         document.getElementById("output").innerHTML = dash;
-        console.log(dash);
+        if(dash.includes('-') != true){
+            document.getElementById("message").innerHTML = 'You win! Start a new game!';
+            document.getElementById("makeGuessButton").innerHTML = '';
+            document.getElementById("letterMenu").innerHTML = '';
+        }
     }else{
         guesses += 1;
         wrongLetters.push(document.getElementById("letterBox").value);
@@ -65,6 +70,12 @@ function handleGuess(){
     document.getElementById("wrongLetters").innerHTML = 'Guessed Letter: ' + wrongLetters;
     hangmanImage();
     document.getElementById("image").innerHTML = "<img src= " + "'" + hangmanImage() + "'" + "/>";
+
+    if(guesses == 10){
+        document.getElementById("message").innerHTML = 'You loose! HA! HA! HA! Start a new game!';
+        document.getElementById("makeGuessButton").innerHTML = '';
+        document.getElementById("letterMenu").innerHTML = '';
+    }
 }
 
 function startGame(){
@@ -73,7 +84,7 @@ function startGame(){
     document.getElementById("wrongLetters").innerHTML = '';
     document.getElementById("message").innerHTML = '';
     getWord();
-    document.getElementById("output").innerHTML = word + wordDisplay();
+    document.getElementById("output").innerHTML = wordDisplay();
     document.getElementById("image").innerHTML = "<img src= " + "'" + hangmanImage() + "'" + "/>";
     document.getElementById("letterMenu").innerHTML = "<select id = 'letterBox' onclick='letterBox()'>\n" +
         "        </select>";
